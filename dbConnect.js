4ef07695eb9eb3c4528806973026c1a14c83504b
princid <princid85@gmail.com>
2023-04-14 23:19:18 +0530

    Latest Commits

--- dbConnect.js
+++ dbConnect.js
@@ -1,18 +1,13 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mjocc.mongodb.net/test`,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+const connection = mongoose.connection;
 
-const connection = mongoose.connection
-
-connection.on('connected', () =>
-  console.log('Mongo DB Connection Successfull')
-);
-
-connection.on('connected' , () => console.log('Mongo DB Connection Successfull'))
+connection.on("connected", () => console.log("Mongo DB Connection Successful"));
